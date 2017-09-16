85e2e38d7542727c3342420eb309427e207b6be6
Druotic <jamesjbeavers@gmail.com>
2017-09-15 20:01:11 -0400

    Add hooks, fix up bash, management port for rabbit

--- deploy-local.sh
+++ deploy-local.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-# explicitly select services to deploy
+# Explicitly select services to deploy
 # since the same compose file may end
-# being used to run integration test containers
+# being used to run integration test containers.
+# Must explicitly start dependencies or they will keep
+# will keep running on ctrl+c. That or switch to just `docker-compose up` and
+# use a separate docker-compose.test.yml
 docker-compose up \
   windbreaker \
+  windbreaker-hooks \
   rss-watcher \
-  npm-watcher
+  npm-watcher \
+  postgres \
+  rabbitmq \
+  rediscluster
